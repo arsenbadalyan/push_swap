cf0ca59df7b0b5fdf54a5df2d451e5163b7184be
Arsen Badalyan <arsenbadalyan.01@gmail.com>
2023-03-01 11:11:17 +0400

    fix:little fixes in checker

--- bonus/checker/checker.c
+++ bonus/checker/checker.c
@@ -7,7 +7,7 @@ void print(char **list)
 
 	i = 0;
 	puts("-----------------");
-	while (list[i+2])
+	while (list[i])
 	{
 		printf("%s", list[i]);
 		i++;
@@ -15,9 +15,9 @@ void print(char **list)
 	printf("%s\n", list[i]);
 }
 
-int	ft_strcmp(const char *str1, const char *str2, size_t n)
+int ft_strcmp(const char *str1, const char *str2, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
 	while ((str1[i] || str2[i]) && i < n)
@@ -35,11 +35,11 @@ char **copy_list(char **list, char *line)
 	size_t i;
 
 	i = 1;
-	while(list[i])
+	while (list[i])
 		i++;
 	new_list = (char **)malloc(sizeof(char *) * (i + 1));
 	i = 0;
-	while(list[i])
+	while (list[i])
 	{
 		new_list[i] = ft_strdup(list[i]);
 		i++;
@@ -47,7 +47,6 @@ char **copy_list(char **list, char *line)
 	new_list[i] = ft_strdup(line);
 	i++;
 	new_list[i] = NULL;
-	free(list);
 	// free_me(0, list);
 	return (new_list);
 }
@@ -57,27 +56,66 @@ char **get_lines(char *line, char **list)
 	line = get_next_line(0);
 	if (!line)
 		return (0);
-	if (!(ft_strcmp(line, "sa", 2) || ft_strcmp(line, "sb", 2)
-		|| ft_strcmp(line, "ss", 2) || ft_strcmp(line, "pa", 2)
-		|| ft_strcmp(line, "pb", 2) || ft_strcmp(line, "ra", 2)
-		|| ft_strcmp(line, "rb", 2) || ft_strcmp(line, "rr", 2)
-		|| ft_strcmp(line, "rra", 3) || ft_strcmp(line, "rrb", 3)
-		|| ft_strcmp(line, "rrr", 3)))
+	if (!(ft_strcmp(line, "sa", 2) || ft_strcmp(line, "sb", 2) || ft_strcmp(line, "ss", 2) || ft_strcmp(line, "pa", 2) || ft_strcmp(line, "pb", 2) || ft_strcmp(line, "ra", 2) || ft_strcmp(line, "rb", 2) || ft_strcmp(line, "rr", 2) || ft_strcmp(line, "rra", 3) || ft_strcmp(line, "rrb", 3) || ft_strcmp(line, "rrr", 3)))
 		return (0);
-	// print(list);
 	list = copy_list(list, line);
-	// print(list);
 	return (list);
+}
+
+void check_sort(t_stack stack_a, char **commands)
+{
+	t_stack *stack_b;
+
+	stack_b = init_stack('b');
+	if (!stack_b)
+		return (0);
+	
+	free_stack(stack_a);
+	free_stack(stack_b);
+}
+
+void check_input(int argc, char **argv, char **commands)
+{
+	int *list;
+	size_t argument_size;
+	t_stack *istack_a;
+
+	istack_a = init_stack('a');
+	if (!istack_a || argc <= 1)
+		return (1);
+	argument_size = validate_arguments(argc, argv);
+	if (argument_size)
+	{
+		list = mkarr_check_doubles(argv, argc, argument_size);
+		if (!list)
+		{
+			free_me(0, commands);
+			free_error(istack_a, 0);
+		}
+		create_stack(list, argument_size, istack_a);
+		if (!istack_a->has_error)
+			return (1);
+	}
+	else
+		free_error(istack_a, 0);
+	free_me(0, commands);
+	return (0);
 }
 
 int main(int argc, char **argv)
 {
 	char *line;
 	char **list;
+	char **temp;
 
 	list = (char **)malloc(sizeof(char *));
-	while(list)
+	while (list)
+	{
 		list = get_lines(line, list);
-	print(list);
+		if (list)
+			temp = list;
+	}
+	list = temp;
+	check_input(argc, argv, list);
 	return (0);
 }
